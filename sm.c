5629dffa2693b5867ef44312c93c5241eecbd3ea
unknown <adrianp126p@gmail.com>
2025-05-11 20:02:46 +0200

    allman format

--- sm.c
+++ sm.c
@@ -12,29 +12,51 @@
 #include "string.h"
 
 #if SM_TICK_FROM_FUNC
+
 SM_TIME_t (*SM_get_tick)(void) = NULL;
-#define SM_GET_TICK ((SM_get_tick!=NULL)?SM_get_tick():((SM_TIME_t)0))
+
+#define SM_GET_TICK    ((SM_get_tick != NULL) ? SM_get_tick() : ((SM_TIME_t)0))
+
 SM_operate_status SM_tick_function_register(SM_TIME_t (*Function)(void))
 {
-	if(NULL==Function) return SM_INIT_ERR;
-	SM_get_tick = Function;
-	return SM_OK;
+    if (Function == NULL)
+    {
+        return SM_INIT_ERR;
+    }
+
+    SM_get_tick = Function;
+    return SM_OK;
 }
+
 #else
+
 SM_TIME_t *SM_tick = NULL;
-#define SM_GET_TICK (*SM_tick)
+
+#define SM_GET_TICK    (*(SM_tick))
+
 SM_operate_status SM_tick_variable_register(SM_TIME_t *Variable)
 {
-	if(NULL==Variable) return SM_INIT_ERR;
-	SM_tick = Variable;
-	return SM_OK;
+    if (Variable == NULL)
+    {
+        return SM_INIT_ERR;
+    }
+
+    SM_tick = Variable;
+    return SM_OK;
 }
+
 #endif
 
-SM_operate_status SM_init(SM_instance_t *SM_instance, SM_state_t *SM_states, uint16_t FirstState, uint16_t NumberOfStates, void *ctx)
+SM_operate_status SM_init(SM_instance_t *SM_instance,
+                          SM_state_t *SM_states,
+                          uint16_t FirstState,
+                          uint16_t NumberOfStates,
+                          void *ctx)
 {
-
-    if(NULL == SM_instance || NULL == SM_states || NumberOfStates <= FirstState || 0 == NumberOfStates)
+    if ((SM_instance == NULL) ||
+        (SM_states == NULL) ||
+        (NumberOfStates <= FirstState) ||
+        (NumberOfStates == 0))
     {
         return SM_INIT_ERR;
     }
@@ -48,35 +70,48 @@ SM_operate_status SM_init(SM_instance_t *SM_instance, SM_state_t *SM_states, uin
     return SM_OK;
 }
 
-SM_operate_status SM_onBreakTimeout_callback_register(SM_instance_t *SM_instance, void (*onBreakTimeout)(SM_instance_t *me))
+SM_operate_status SM_onBreakTimeout_callback_register(SM_instance_t *SM_instance,
+                                                      void (*onBreakTimeout)(SM_instance_t *me))
 {
-	if(NULL==SM_instance) return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    if (SM_instance == NULL)
+    {
+        return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    }
 
-	SM_instance->onBreakTimeout = onBreakTimeout;
-	return SM_OK;
+    SM_instance->onBreakTimeout = onBreakTimeout;
+    return SM_OK;
 }
 
-SM_operate_status SM_onTrans_callback_register(SM_instance_t *SM_instance, void (*onTrans)(SM_instance_t *me))
+SM_operate_status SM_onTrans_callback_register(SM_instance_t *SM_instance,
+                                               void (*onTrans)(SM_instance_t *me))
 {
-	if(NULL==SM_instance) return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    if (SM_instance == NULL)
+    {
+        return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    }
 
-	SM_instance->onTrans = onTrans;
-	return SM_OK;
+    SM_instance->onTrans = onTrans;
+    return SM_OK;
 }
 
 SM_operate_status SM_Execution(SM_instance_t *SM_instance)
 {
-    if(SM_instance->SM_control_flags.ExecBreak && (SM_GET_TICK - SM_instance->Time.ExecBlockTick >= SM_instance->Time.ExecBlockTimeout))
+    if (SM_instance->SM_control_flags.ExecBreak &&
+        (SM_GET_TICK - SM_instance->Time.ExecBlockTick >= SM_instance->Time.ExecBlockTimeout))
     {
         SM_instance->SM_control_flags.ExecBreak = 0;
-        if(NULL != SM_instance->onBreakTimeout) SM_instance->onBreakTimeout(SM_instance);
+
+        if (SM_instance->onBreakTimeout != NULL)
+        {
+            SM_instance->onBreakTimeout(SM_instance);
+        }
     }
 
-    if(NULL != SM_instance->ActualState->onExec)
+    if (SM_instance->ActualState->onExec != NULL)
     {
-        if(((0 == SM_instance->Time.DelayTime) ||
-        (SM_GET_TICK-SM_instance->Time.lastExecTick >= SM_instance->Time.DelayTime)) &&
-        !SM_instance->SM_control_flags.ExecBreak)
+        if (((SM_instance->Time.DelayTime == 0) ||
+             (SM_GET_TICK - SM_instance->Time.lastExecTick >= SM_instance->Time.DelayTime)) &&
+            !SM_instance->SM_control_flags.ExecBreak)
         {
             SM_instance->Time.lastExecTick = SM_GET_TICK;
             SM_instance->Time.DelayTime = 0;
@@ -85,142 +120,218 @@ SM_operate_status SM_Execution(SM_instance_t *SM_instance)
             SM_instance->Stats.MachineExecutionCounter++;
             return SM_OK;
         }
-        else return SM_EXEC_DELAYED;
+        else
+        {
+            return SM_EXEC_DELAYED;
+        }
     }
-    else return SM_EXEC_NULL_PTR;
+    else
+    {
+        return SM_EXEC_NULL_PTR;
+    }
 }
 
 SM_operate_status SM_Trans(SM_instance_t *SM_instance, SM_transision_mode mode, uint16_t State)
 {
-    if(SM_instance->NumberOfStates <= State) return SM_TRANS_ERR;
-    if(SM_instance->SM_control_flags.TransitionLock)
+    if (SM_instance->NumberOfStates <= State)
     {
-    	if(SM_GET_TICK - SM_instance->Time.ExecBlockTick >= SM_instance->Time.TransLockTimeout)
-    	{
-    		SM_instance->SM_control_flags.TransitionLock = 0;
-    	}
-    	else
-    	{
-    		return SM_TRANS_LOCKED;
-    	}
+        return SM_TRANS_ERR;
     }
 
-    switch(mode)
+    if (SM_instance->SM_control_flags.TransitionLock)
+    {
+        if (SM_GET_TICK - SM_instance->Time.ExecBlockTick >= SM_instance->Time.TransLockTimeout)
+        {
+            SM_instance->SM_control_flags.TransitionLock = 0;
+        }
+        else
+        {
+            return SM_TRANS_LOCKED;
+        }
+    }
+
+    switch (mode)
     {
         case SM_TRANS_ENTRY_EXIT:
-            if((NULL!=SM_instance->ActualState->onExit) || NULL!=SM_instance->ActualState->onEntry)
+        {
+            if ((SM_instance->ActualState->onExit != NULL) || (SM_instance->ActualState->onEntry != NULL))
             {
                 SM_instance->ActualState->onExit(SM_instance);
             }
-            else return SM_TRANS_ERR;
+            else
+            {
+                return SM_TRANS_ERR;
+            }
+
             SM_instance->PrevState = SM_instance->ActualState;
             SM_instance->ActualState = &SM_instance->SM_states[State];
             SM_instance->ActualState->onEntry(SM_instance);
-        break;
+            break;
+        }
+
         case SM_TRANS_ENTRY:
-            if(NULL==SM_instance->ActualState->onEntry) return SM_TRANS_ERR;
+        {
+            if (SM_instance->ActualState->onEntry == NULL)
+            {
+                return SM_TRANS_ERR;
+            }
+
             SM_instance->PrevState = SM_instance->ActualState;
             SM_instance->ActualState = &SM_instance->SM_states[State];
             SM_instance->ActualState->onEntry(SM_instance);
-        break;
+            break;
+        }
+
         case SM_TRANS_EXIT:
-            if((NULL!=SM_instance->ActualState->onExit))
+        {
+            if (SM_instance->ActualState->onExit != NULL)
             {
                 SM_instance->ActualState->onExit(SM_instance);
             }
-            else return SM_TRANS_ERR;
+            else
+            {
+                return SM_TRANS_ERR;
+            }
+
             SM_instance->PrevState = SM_instance->ActualState;
             SM_instance->ActualState = &SM_instance->SM_states[State];
-        break;
+            break;
+        }
+
         case SM_TRANS_FAST:
+        {
             SM_instance->PrevState = SM_instance->ActualState;
             SM_instance->ActualState = &SM_instance->SM_states[State];
-        break;
+            break;
+        }
+
+        default:
+        {
+            return SM_TRANS_ERR;
+        }
     }
 
     SM_instance->Time.TransTick = SM_GET_TICK;
-    if(NULL != SM_instance->onTrans) SM_instance->onTrans(SM_instance);
+
+    if (SM_instance->onTrans != NULL)
+    {
+        SM_instance->onTrans(SM_instance);
+    }
+
     SM_instance->Stats.StateExecutionCounter = 0;
     SM_instance->Stats.TransCounter++;
+
     return SM_OK;
 }
 
 SM_operate_status SM_exec_break(SM_instance_t *SM_instance, SM_TIME_t Timeout)
 {
-    if(NULL==SM_instance) return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    if (SM_instance == NULL)
+    {
+        return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    }
+
     SM_instance->Time.ExecBlockTick = SM_GET_TICK;
     SM_instance->Time.ExecBlockTimeout = Timeout;
     SM_instance->SM_control_flags.ExecBreak = 1;
+
     return SM_OK;
 }
 
 SM_operate_status SM_exec_break_release(SM_instance_t *SM_instance)
 {
-    if(NULL==SM_instance) return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    if (SM_instance == NULL)
+    {
+        return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    }
+
     SM_instance->SM_control_flags.ExecBreak = 0;
     return SM_OK;
 }
 
 SM_operate_status SM_trans_lock(SM_instance_t *SM_instance, SM_TIME_t Timeout)
 {
-    if(NULL==SM_instance) return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    if (SM_instance == NULL)
+    {
+        return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    }
+
     SM_instance->Time.TransLockTick = SM_GET_TICK;
     SM_instance->Time.TransLockTimeout = Timeout;
     SM_instance->SM_control_flags.TransitionLock = 1;
+
     return SM_OK;
 }
 
 SM_operate_status SM_trans_lock_release(SM_instance_t *SM_instance)
 {
-    if(NULL==SM_instance) return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    if (SM_instance == NULL)
+    {
+        return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    }
+
     SM_instance->SM_control_flags.TransitionLock = 0;
     return SM_OK;
 }
 
 SM_operate_status SM_exec_delay(SM_instance_t *SM_instance, SM_TIME_t Delay)
 {
-    if(NULL==SM_instance) return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    if (SM_instance == NULL)
+    {
+        return SM_OPRT_INSTANCE_DOES_NOT_EXIST;
+    }
+
     SM_instance->Time.DelayTime = Delay;
     return SM_OK;
 }
 
 uint16_t SM_get_state_number(SM_instance_t *SM_instance)
 {
-    if (!SM_instance || !SM_instance->ActualState) return 0xFFFF;
+    if ((SM_instance == NULL) || (SM_instance->ActualState == NULL))
+    {
+        return UINT16_MAX;
+    }
 
-    return SM_instance->ActualState - SM_instance->SM_states;
+    return (uint16_t)(SM_instance->ActualState - SM_instance->SM_states);
 }
 
 SM_TIME_t SM_get_time_in_state(SM_instance_t *SM_instance)
 {
-	if (!SM_instance || !SM_instance->ActualState) return 0xFFFF;
+    if ((SM_instance == NULL) || (SM_instance->ActualState == NULL))
+    {
+        return SM_MAX_TIMEOUT;
+    }
 
-	return SM_GET_TICK - SM_instance->Time.TransTick;
+    return (SM_GET_TICK - SM_instance->Time.TransTick);
 }
 
 uint32_t SM_get_exec_counter_state(SM_instance_t *SM_instance)
 {
-	if (!SM_instance || !SM_instance->ActualState) return 0xFFFF;
+    if ((SM_instance == NULL) || (SM_instance->ActualState == NULL))
+    {
+        return UINT32_MAX;
+    }
 
-	return SM_instance->Stats.StateExecutionCounter;
+    return SM_instance->Stats.StateExecutionCounter;
 }
 
 uint32_t SM_get_exec_counter_machine(SM_instance_t *SM_instance)
 {
-	if (!SM_instance) return 0xFFFF;
+    if (SM_instance == NULL)
+    {
+        return UINT32_MAX;
+    }
 
-	return SM_instance->Stats.MachineExecutionCounter;
+    return SM_instance->Stats.MachineExecutionCounter;
 }
 
 uint32_t SM_get_trans_counter(SM_instance_t *SM_instance)
 {
-	if (!SM_instance) return 0xFFFF;
+    if (SM_instance == NULL)
+    {
+        return UINT32_MAX;
+    }
 
-	return SM_instance->Stats.TransCounter;
+    return SM_instance->Stats.TransCounter;
 }
-
-
-
-
-
 
--- sm.h
+++ sm.h
@@ -15,106 +15,127 @@
 
 #define SM_TICK_FROM_FUNC 0
 
-
-
 #ifndef SM_TIME_BASE_TYPE_CUSTOM
-#define SM_MAX_TIMEOUT UINT32_MAX
-typedef volatile uint32_t SM_TIME_t;
+
+    #define SM_MAX_TIMEOUT UINT32_MAX
+    typedef volatile uint32_t SM_TIME_t;
+
 #else
 
-#define MAX_OF_TYPE(type) \
-    _Generic((type)0, \
-        int8_t: INT8_MAX, \
-        uint8_t: UINT8_MAX, \
-        int16_t: INT16_MAX, \
-        uint16_t: UINT16_MAX, \
-        int32_t: INT32_MAX, \
-        uint32_t: UINT32_MAX, \
-        int64_t: INT64_MAX, \
-        uint64_t: UINT64_MAX, \
-        default: 0)
+    #define MAX_OF_TYPE(type)                       \
+        _Generic((type)0,                           \
+            int8_t: INT8_MAX,                       \
+            uint8_t: UINT8_MAX,                     \
+            int16_t: INT16_MAX,                     \
+            uint16_t: UINT16_MAX,                   \
+            int32_t: INT32_MAX,                     \
+            uint32_t: UINT32_MAX,                   \
+            int64_t: INT64_MAX,                     \
+            uint64_t: UINT64_MAX,                   \
+            default: 0)
 
-#define SM_MAX_TIMEOUT MAX_OF_TYPE(SM_TIME_BASE_TYPE_CUSTOM)
-typedef SM_TIME_BASE_TYPE_CUSTOM SM_TIME_t;
+    #define SM_MAX_TIMEOUT MAX_OF_TYPE(SM_TIME_BASE_TYPE_CUSTOM)
+    typedef SM_TIME_BASE_TYPE_CUSTOM SM_TIME_t;
+
 #endif
 
-typedef enum{
+typedef enum
+{
     SM_OK = 0,
     SM_OPRT_INSTANCE_DOES_NOT_EXIST,
     SM_INIT_ERR,
     SM_EXEC_DELAYED,
     SM_EXEC_NULL_PTR,
     SM_TRANS_ERR,
-	SM_TRANS_LOCKED
-}SM_operate_status;
+    SM_TRANS_LOCKED
+} SM_operate_status;
 
-typedef enum{
+typedef enum
+{
     SM_TRANS_ENTRY_EXIT = 0,
     SM_TRANS_ENTRY,
     SM_TRANS_EXIT,
     SM_TRANS_FAST
-}SM_transision_mode;
+} SM_transision_mode;
 
 typedef struct SM_instance_t SM_instance_t;
 
-typedef struct{
-	SM_TIME_t TransTick;
-	SM_TIME_t lastExecTick;
-	SM_TIME_t ExecBlockTick;
-	SM_TIME_t TransLockTick;
-	SM_TIME_t DelayTime;
-	SM_TIME_t ExecBlockTimeout;
-	SM_TIME_t TransLockTimeout;
-}SM_timestamp_t;
+typedef struct
+{
+    SM_TIME_t TransTick;
+    SM_TIME_t lastExecTick;
+    SM_TIME_t ExecBlockTick;
+    SM_TIME_t TransLockTick;
+    SM_TIME_t DelayTime;
+    SM_TIME_t ExecBlockTimeout;
+    SM_TIME_t TransLockTimeout;
+} SM_timestamp_t;
 
-typedef struct{
+typedef struct
+{
     void (*onEntry)(SM_instance_t *me);
     void (*onExec)(SM_instance_t *me);
     void (*onExit)(SM_instance_t *me);
-}SM_state_t;
+} SM_state_t;
 
-typedef struct{
-    uint32_t ExecBreak :1;
-    uint32_t TransitionLock :1;
-}SM_control_flags_t;
+typedef struct
+{
+    uint32_t ExecBreak : 1;
+    uint32_t TransitionLock : 1;
+} SM_control_flags_t;
 
-typedef struct{
-	uint32_t StateExecutionCounter, MachineExecutionCounter, TransCounter;
-}SM_stats_t;
+typedef struct
+{
+    uint32_t StateExecutionCounter;
+    uint32_t MachineExecutionCounter;
+    uint32_t TransCounter;
+} SM_stats_t;
 
-struct SM_instance_t{
+struct SM_instance_t
+{
     SM_state_t *SM_states;
     SM_state_t *ActualState;
     SM_state_t *PrevState;
     uint16_t NumberOfStates;
+
     SM_timestamp_t Time;
     SM_control_flags_t SM_control_flags;
     SM_stats_t Stats;
+
     void (*onBreakTimeout)(SM_instance_t *me);
     void (*onTrans)(SM_instance_t *me);
+
     void *ctx;
 };
 
+// Tick source registration
 #if SM_TICK_FROM_FUNC
 SM_operate_status SM_tick_function_register(SM_TIME_t (*Function)(void));
 #else
 SM_operate_status SM_tick_variable_register(SM_TIME_t *Variable);
 #endif
 
+// Core API
 SM_operate_status SM_init(SM_instance_t *SM_instance, SM_state_t *SM_states, uint16_t FirstState, uint16_t NumberOfStates, void *ctx);
-SM_operate_status SM_onBreakTimeout_callback_register(SM_instance_t *SM_instance, void (*onBreakTimeout)(SM_instance_t *me));
-SM_operate_status SM_onTrans_callback_register(SM_instance_t *SM_instance, void (*onTrans)(SM_instance_t *me));
 SM_operate_status SM_Execution(SM_instance_t *SM_instance);
 SM_operate_status SM_Trans(SM_instance_t *SM_instance, SM_transision_mode mode, uint16_t State);
+
+// Control mechanisms
 SM_operate_status SM_exec_break(SM_instance_t *SM_instance, SM_TIME_t Timeout);
 SM_operate_status SM_exec_break_release(SM_instance_t *SM_instance);
 SM_operate_status SM_trans_lock(SM_instance_t *SM_instance, SM_TIME_t Timeout);
 SM_operate_status SM_trans_lock_release(SM_instance_t *SM_instance);
 SM_operate_status SM_exec_delay(SM_instance_t *SM_instance, SM_TIME_t Delay);
-uint16_t SM_get_state_number(SM_instance_t *SM_instance);
-SM_TIME_t SM_get_time_in_state(SM_instance_t *SM_instance);
-uint32_t SM_get_exec_counter_state(SM_instance_t *SM_instance);
-uint32_t SM_get_exec_counter_machine(SM_instance_t *SM_instance);
-uint32_t SM_get_trans_counter(SM_instance_t *SM_instance);
+
+// Callbacks
+SM_operate_status SM_onBreakTimeout_callback_register(SM_instance_t *SM_instance, void (*onBreakTimeout)(SM_instance_t *me));
+SM_operate_status SM_onTrans_callback_register(SM_instance_t *SM_instance, void (*onTrans)(SM_instance_t *me));
+
+// Introspection
+uint16_t    SM_get_state_number(SM_instance_t *SM_instance);
+SM_TIME_t   SM_get_time_in_state(SM_instance_t *SM_instance);
+uint32_t    SM_get_exec_counter_state(SM_instance_t *SM_instance);
+uint32_t    SM_get_exec_counter_machine(SM_instance_t *SM_instance);
+uint32_t    SM_get_trans_counter(SM_instance_t *SM_instance);
 
 #endif /* SM_CORE_SM_H_ */
